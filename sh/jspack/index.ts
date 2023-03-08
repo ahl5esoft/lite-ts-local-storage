1e9000ce6afff60d172d3f9f9bf5866326ee380b
dengwenbin <dengwenbin@fengling.cn>
2023-03-08 17:35:11 +0800

    feat: 升级lite-ts-fs

--- sh/jspack/index.ts
+++ sh/jspack/index.ts
@@ -1,19 +1,6 @@
-import { FileFactory, JsPack } from 'lite-ts-fs';
+import { JsPack } from 'lite-ts-fs';
 
 (async () => {
     const jsPack = new JsPack();
-    const res = await jsPack.getDirContent('dist');
-
-    const fsFactory = new FileFactory();
-    const pkg = await fsFactory.buildFile('package.json').read<{ name: string; }>();
-
-    await fsFactory.buildFile(`${pkg.name}.d.ts`).write(
-        res.join('\n').replace(/export\ /g, '')
-            .replace(/moment\.unitOfTime\.StartOf/g, 'string')
-    );
-
-    const licenseFile = fsFactory.buildFile(`${pkg.name}.min.js.LICENSE.txt`);
-    const exists = await licenseFile.exists();
-    if (exists)
-        await licenseFile.remove();
+    await jsPack.pack()
 })();
