49911c3fbb9c36f1f2135c2faa69e3032d309398
linyimin <ahl5esoft@gmail.com>
2023-03-24 16:30:38 +0800

    docs: v1.5.1

--- src/model-db-option.ts
+++ src/model-db-option.ts
@@ -5,8 +5,9 @@ import { DbQuery } from './db-query';
 
 export function modelDbOption<T>(typer: new () => T): DbOption {
     return (dbFactory: LocalStorageDbFactory, dbRepo: DbRepository<DbModel>) => {
+        dbRepo.model =(typer as any).ctor ?? typer.name;
         dbRepo.createQueryFunc = () => {
-            return new DbQuery(dbFactory.localStorage, (typer as any).ctor ?? typer.name);
+            return new DbQuery(dbFactory.localStorage, dbRepo.model);
         };
     };
 }
