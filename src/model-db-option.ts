16c85431a8f2971b6bf29480185f33a8e536b5d2
ahl5esoft <ahl5esoft@gmail.com>
2023-04-16 18:57:40 +0800

    refactor: 删除jspack

--- src/model-db-option.ts
+++ src/model-db-option.ts
@@ -1,11 +1,12 @@
 import { DbModel, DbOption, DbRepository } from 'lite-ts-db';
+import { ioc } from 'lite-ts-ioc';
 
 import { LocalStorageDbFactory } from './db-factory';
 import { DbQuery } from './db-query';
 
 export function modelDbOption<T>(typer: new () => T): DbOption {
     return (dbFactory: LocalStorageDbFactory, dbRepo: DbRepository<DbModel>) => {
-        dbRepo.model =(typer as any).ctor ?? typer.name;
+        dbRepo.model = ioc.getKey(typer);
         dbRepo.createQueryFunc = () => {
             return new DbQuery(dbFactory.localStorage, dbRepo.model);
         };
